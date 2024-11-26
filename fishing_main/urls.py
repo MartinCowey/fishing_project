3f7c6774a10b56b96794c5dbdced7cc02f316286
Martin Cowey <starman.martin@gmail.com>
2024-11-26 11:40:38 +0000

    Ready for deploy

--- fishing_main/urls.py
+++ fishing_main/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog.views import my_blog
+from django.urls import path, include
+from blog import views as blog_views
+
+# from blog.views import my_blog
 
 urlpatterns = [
+    path("blog/", blog_views.my_blog, name='blog'),
     path('admin/', admin.site.urls),
-    path('blog/', my_blog, name='blog'),
+    # path('blog/', my_blog, name='blog'),
 ]
