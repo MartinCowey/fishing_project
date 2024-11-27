8f6ea0a387f13612fc1d78b2eefe333ccd2d5d63
Martin Cowey <starman.martin@gmail.com>
2024-11-27 12:21:02 +0000

    enable serving of static files

--- fishing_main/settings.py
+++ fishing_main/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = ['8000-martincowey-fishingproj-o68dhk2q1jr.ws.codeinstitute-ide.net',
 '.herokuapp.com',]
