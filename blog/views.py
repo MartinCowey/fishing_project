ec030d71641741d207fccfc824fada1d7e0cef0b
Martin Cowey <starman.martin@gmail.com>
2024-11-30 18:09:03 +0000

    refactor templates for less repeated code

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,3 @@
-# from django.shortcuts import render
-# from django.http import HttpResponse
-# from django.views.generic import TemplateView
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.generic import TemplateView
