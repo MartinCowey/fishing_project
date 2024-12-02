32ff9a6d109bd55a6ffc49994e3207062a8e69e1
Martin Cowey <starman.martin@gmail.com>
2024-12-02 18:45:05 +0000

    Add a create new comment section

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.generic import TemplateView, ListView
 from .models import Blog
+from .forms import CommentForm
 from comments.forms import CommentForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -25,11 +27,26 @@ def home_page(request):
 
 def blog_post(request, slug):
     blog = get_object_or_404(Blog, slug=slug)
-    comments = blog.comments.filter(approved=True)
+    comments = blog.comments.all().order_by("-created_on")
+    comment_count = blog.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.blog = blog
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+
     comment_form = CommentForm()
+
     return render(request, 'blog/blog_post.html', {
         'blog': blog,
         'comments': comments,
-        'comment_form': comment_form
+        'comment_form': comment_form,
+        'comment_count': comment_count,
     })
 
--- comments/forms.py
+++ comments/forms.py
@@ -5,3 +5,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+        labels = {
+            'body': '',  # This removes the label for the 'body' field
+        }
+        widgets = {
+            'body': forms.Textarea(attrs={'placeholder': 'Enter your comment here...'})
+        }
