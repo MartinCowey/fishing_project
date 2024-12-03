724dbe77a45f9ec528362678da991dff637169c2
Martin Cowey <starman.martin@gmail.com>
2024-12-03 17:08:35 +0000

    Add d the delete functionality for the comments section

--- comments/urls.py
+++ comments/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('add/<slug:slug>/', views.add_comment, name='add_comment'),
     path('<slug:slug>/edit_comment/<int:comment_id>/',
          views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
