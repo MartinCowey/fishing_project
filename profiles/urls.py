49d8cd02eb43a3e2df06f61c24f26b3e4710ee34
Martin Cowey <starman.martin@gmail.com>
2024-12-09 14:08:31 +0000

    Update clean code for responsive design, screen readers and accesibility for the blog posting

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import ProfileDetailView, ProfileUpdateView, ProfileCreateView, ProfileDeleteView, CustomSignupView
+from . import views  # Import the views module
 
 urlpatterns = [
     path('signup/', CustomSignupView.as_view(), name='account_signup'),  # Use custom signup view here
